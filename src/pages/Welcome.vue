31cffc8d20e28bd789076e24a2bce321d842c110
Salvador Martinez <cartoonrocket9@gmail.com>
2021-05-29 00:21:14 +0200

    Created "Welcome.vue" Page

---
+++ src/pages/Welcome.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-page class="inline-block absolute-center ">
+
+    <div>
+      <h2 class="welcome-class text-white">hello...</h2>
+    </div>
+    <div>
+      <small class="name-class absolute-center" >enter your name</small>
+    </div>
+    <div>
+      <q-input autofocus rounded standout />
+    </div>
+
+
+
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .welcome-class {
+    margin-top: 120px;
+    text-align: center;
+    font-family: 'pattaya';
+    font-size: 77px;
+  }
+  .name-class {
+  margin-top: 60px;
+  text-align: center;
+  font-family: 'pattaya';
+  font-size: 77px;
+}
+  .q-input {
+    text-align: center;
+    min-width: 400px;
+    max-width: 500px;
+  }
+</style>

ea351c6e1ba4c24fb7fff2d0a4f612baf1f762c9
Salvador Martinez <cartoonrocket9@gmail.com>
2021-05-29 00:22:18 +0200

    Declared "Layout" & "Welcome Page" at routes file

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,9 +2,9 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('src/layouts/Layout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Welcome.vue') }
     ]
   },
 

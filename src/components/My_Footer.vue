5e01c3c3ced7d077d6bd3968244c0acbea0eb9f4
Salvador Martinez <cartoonrocket9@gmail.com>
2021-05-29 11:13:50 +0200

    Created a Component for the footer

---
+++ src/components/My_Footer.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <q-footer elevated class="col text-deep-orange-11">
+
+      <q-toolbar class="text-center q-mt-sm">
+        <q-toolbar-title>{{ title }}</q-toolbar-title>
+      </q-toolbar>
+
+      <q-toolbar class="text-center q-mb-md">
+        <q-toolbar-title>
+          <span class="toolbar-title_class">
+            By: {{ author }}
+          </span>
+        </q-toolbar-title>
+      </q-toolbar>
+
+    </q-footer>
+
+</template>
+
+<script>
+export default {
+    props: [
+        'title',
+        'author'
+    ]
+}
+</script>
+
+<style>
+
+</style>

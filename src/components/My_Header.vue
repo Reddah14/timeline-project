84baead7f1e2c141235a792cc97b9aa4c3c30363
Salvador Martinez <cartoonrocket9@gmail.com>
2021-05-29 10:36:33 +0200

    Created a Component for the header

---
+++ src/components/My_Header.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title class="text-center glossy">
+          <slot></slot>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+ .toolbar-title_class {
+   font-family: 'pattaya';
+   letter-spacing: 1px;
+ }
+</style>

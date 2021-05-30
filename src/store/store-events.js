122f76eb0a313fe7cd8f8eab16a51a28684035a8
Salvador Martinez <cartoonrocket9@gmail.com>
2021-05-30 15:59:23 +0200

    *VUEX* created an action & mutation in order to Edit the selected Event

--- src/store/store-events.js
+++ src/store/store-events.js
@@ -19,14 +19,21 @@ const state = {
 			title: 'London trip',
 			subtitle: 'March 14, 2020',
 			body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-		},                        
+		},            
 	]
 }
 
 const mutations = {
     addEvent(state, event) {
         state.events.push(event)
-    }
+    },
+	updateEvent(state, event) {
+        for(var i=0; i< state.events.length; i++) {
+            if ( state.events[i].id == event.id ) {
+                Object.assign( state.events[i], event )
+            }
+        }
+	}
 }
 
 const actions = {
@@ -36,7 +43,10 @@ const actions = {
 
 // now we commit the mutation
         commit('addEvent', event)
-    }
+    },
+	updateEvent({ commit }, event) {
+		commit('updateEvent', event)
+	}
 }
 
 const getters = {

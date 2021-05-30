919db57217d7ed3b2c056592dd1c2a7d914286c4
Salvador Martinez <cartoonrocket9@gmail.com>
2021-05-30 10:29:02 +0200

    *VUEX* Added the events data to the state in the store module & created a getter for those events

--- src/store/store-events.js
+++ src/store/store-events.js
@@ -1,5 +1,24 @@
 const state = {
-
+	events: [
+		{
+			id: 1,
+			title: 'I was born',
+			subtitle: 'February 8, 1986',
+			body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+		},
+		{
+			id: 2,
+			title: '1st trip home',
+			subtitle: 'February 21, 1986',
+			body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+		},
+		{
+			id: 3,
+			title: 'London trip',
+			subtitle: 'March 14, 2020',
+			body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+		},                        
+	]
 }
 
 const mutations = {
@@ -11,10 +30,13 @@ const actions = {
 }
 
 const getters = {
-
+    events: (state) => {
+        return state.events
+    }
 }
 
 export default {
+	namespaced: true,
 	state,
 	mutations,
 	actions,

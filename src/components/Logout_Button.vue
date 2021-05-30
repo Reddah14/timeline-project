0b412eab67ed34a527d934daa48a05949658ab79
Salvador Martinez <cartoonrocket9@gmail.com>
2021-05-30 23:52:01 +0200

    Created Component for Logout Button

---
+++ src/components/Logout_Button.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <q-btn
+        @click="LogoutUser"
+        style="max-width: 30px;"
+        padding="10px 5px"
+        color="secondary"
+        text-color="dark"
+        icon="logout"
+    >
+        <q-tooltip content-class="bg-accent">
+          Log me Out !
+        </q-tooltip>
+    
+    </q-btn>
+
+</template>
+
+<script>
+export default {
+    methods: {
+        LogoutUser() {
+            this.$objToaster.Toastify_ByeBye(this.$q.sessionStorage.getItem( 'userName' ))
+            this.$objFunctions.removeUserName(this.user);
+            this.$router.push({ path: '/' });
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

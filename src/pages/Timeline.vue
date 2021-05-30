1a37ae19458b8e22c0f48c2f208edb7ecac6c7b7
Salvador Martinez <cartoonrocket9@gmail.com>
2021-05-30 10:30:50 +0200

    Mapped our events getter & Able to reproduce them in layout

--- src/pages/Timeline.vue
+++ src/pages/Timeline.vue
@@ -35,31 +35,12 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  data () {
-    return {
-        events: [
-            {
-                id: 1,
-                title: 'I was born',
-                subtitle: 'February 8, 1986',
-                body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-            },
-            {
-                id: 2,
-                title: '1st trip home',
-                subtitle: 'February 21, 1986',
-                body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-            },
-            {
-                id: 3,
-                title: 'London trip',
-                subtitle: 'March 14, 2020',
-                body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-            },                        
-        ]
+    computed: {
+        ...mapGetters('events', ['events'])
     }
-  }
 }
 </script>
 

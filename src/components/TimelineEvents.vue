ca37dd03b7225705c23e7f9f2fdc15ab28986db4
Salvador Martinez <cartoonrocket9@gmail.com>
2021-05-30 14:09:16 +0200

    Created Component for our Timeline Events

---
+++ src/components/TimelineEvents.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <q-timeline
+        class="q-px-md"
+        color="secondary"
+        side="left"
+        layout="comfortable"
+        dark
+    >
+        
+        <q-timeline-entry heading body="Timeline Events" />
+
+        <q-timeline-entry
+            v-for="event in events"
+            :key="event.id"
+            :title="event.title"
+            :subtitle="event.subtitle"
+            :body="event.body"
+        >
+            <q-btn
+                class="row text-center"
+                padding="sm"
+                round
+                color="deep-orange"
+                icon="edit" 
+            />
+        </q-timeline-entry>
+
+    </q-timeline>
+
+</template>
+
+<script>
+export default {
+    props: ['events']
+}
+</script>
+
+<style>
+
+</style>

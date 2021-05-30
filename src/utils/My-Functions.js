dc9e450da77aac7192cc6c0be3798df3340147ef
Salvador Martinez <cartoonrocket9@gmail.com>
2021-05-30 23:53:19 +0200

    Added method to remove user name from session storage

--- src/utils/My-Functions.js
+++ src/utils/My-Functions.js
@@ -4,6 +4,9 @@ const objFunctions = new Vue({
     methods: {
         saveUserName(_name) {
             this.$q.sessionStorage.set( 'userName', _name );
+        },
+        removeUserName() {
+            this.$q.sessionStorage.remove( 'userName' );
         }
     }
 })

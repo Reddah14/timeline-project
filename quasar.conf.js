671088dd984f1641100eed4543cd54a65cb95ab8
Salvador Martinez <cartoonrocket9@gmail.com>
2021-06-02 16:38:31 +0200

    Add 'Dialog' plugin

--- quasar.conf.js
+++ quasar.conf.js
@@ -96,7 +96,8 @@ module.exports = function (/* ctx */) {
       // Quasar plugins
       plugins: [
         'Notify',
-        'SessionStorage'
+        'SessionStorage',
+        'Dialog'
       ]
     },
 

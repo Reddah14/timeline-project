0e79754ca94db498b07ec96056e9cfaa5618c409
Salvador Martinez <cartoonrocket9@gmail.com>
2021-06-02 13:19:19 +0200

    Delete mock events from events store module

--- src/store/store-events.js
+++ src/store/store-events.js
@@ -1,26 +1,7 @@
 import { uid } from 'quasar'
 
 const state = {
-	events: [
- 		{
-			id: 1,
-			title: 'I was born',
-			subtitle: 'February 8, 1986',
-			body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-		},
-		{
-			id: 2,
-			title: '1st trip home',
-			subtitle: 'February 21, 1986',
-			body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-		},
-		{
-			id: 3,
-			title: 'London trip',
-			subtitle: 'March 14, 2020',
-			body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-		}
-	]
+	events: []
 }
 
 const mutations = {

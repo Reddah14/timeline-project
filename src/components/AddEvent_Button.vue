3446f47d6fc52818d5e4f98329042d1e04ddcb3c
Salvador Martinez <cartoonrocket9@gmail.com>
2021-06-02 13:17:01 +0200

    Created component for Add-Event button

---
+++ src/components/AddEvent_Button.vue
@@ -0,0 +1,12 @@
+<template>
+
+    <q-btn
+        @click="$emit( 'showAddEventDialog' )"
+        padding="sm"
+        color="secondary"
+        text-color="brown-9"
+        icon="add"
+        size="29px"
+    />
+
+</template>
